4369a9524a93c8661af733deaef023b01a25a03a
Morgan Lyle <morganlylee@gmail.com>
2022-04-20 14:06:31 -0500

    first push

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-
+from recipes.models import Recipe
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Recipe, RecipeAdmin)
